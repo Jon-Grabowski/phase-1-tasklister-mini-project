3e811724cf1c99b0bad3bd220aad1a3e487ded5b
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-05-28 17:31:02 -0400

    Test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const taskList = document.getElementById('tasks');
+    let listElement = document.createElement('li');
+    listElement.innerText = e.target['new-task-description'].value;
+    taskList.append(listElement);
+    form.reset();
+
+    button = document.createElement('button');
+    button.innerText = 'X';
+    listElement.append(button);
+    console.log(button)
+    //console.log(e.target['new-task-description'].value);
+
+  });
 });
+
